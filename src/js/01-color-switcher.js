b66b2e97a87df1b2e0a6369bef935b2df9d80297
Andreii-Denysenko <a.denisenko2111@gmail.com>
2022-08-12 22:46:12 +0300

    solution tack 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
+const refs = {
+    btStart: document.querySelector("[data-start]"),
+    btStop: document.querySelector("[data-stop]"),
+};
+
+refs.btStart.addEventListener('click', onStartBodyColor);
+refs.btStop.addEventListener('click', onStopBodyColor);
+refs.btStop.disabled = true;
+let idInterval = null;
+
+function onStartBodyColor(){
+idInterval = setInterval(onRandomColor, 1000);
+ refs.btStart.disabled = true;
+ refs.btStop.disabled = false;
+};
+
+function onRandomColor (){
+document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopBodyColor(){
+clearInterval(idInterval);
+refs.btStart.disabled = false;
+};
