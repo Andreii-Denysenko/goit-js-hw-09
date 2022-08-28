9908500130124d8832175ca6cc2071b1acd11179
Andreii-Denysenko <a.denisenko2111@gmail.com>
2022-08-28 18:42:39 +0300

    update tack 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
-const btn = document.querySelector('button [type="submit"]')
 let delay = document.querySelector('input [name="delay"]');
 let step = document.querySelector('input [name="step"]');
 let amount = document.querySelector('input [name="amount"]');
@@ -18,8 +17,8 @@ form.addEventListener("submit", onGenerationPromise);
     amount = Number(evt.currentTarget.amount.value);
 
    if(delay >= 0 && step >= 0 && amount > 0 ){
-for  (let position = 1; position <= amount; position +=1){
-   delay += step;
+for  (let position = 1; position <= amount; position += 1){
+   
      createPromise(position, delay)
  .then(({ position, delay }) => {
     setTimeout(() => {
@@ -30,19 +29,22 @@ for  (let position = 1; position <= amount; position +=1){
     setTimeout(() => {
         Notify.failure(` Rejected promise ${position} in ${delay}ms`)
     }, delay);
+ })
+ delay += step;
+};
+   }
    
- });
-}
-   }};
+};
 
 function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
     const objectPromise = { position, delay };
-  
     return new Promise((resolve, reject) => {
-      if (shouldResolve) {
+setTimeout(() => {
+ if (shouldResolve) {
         resolve(objectPromise);
       }
       reject(objectPromise);
-    });
-  };
+},delay)
+   });
+   };
